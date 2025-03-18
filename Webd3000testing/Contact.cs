0765f1aca8a0419555d5d00f28f269d6ffea0292
Maris Hume <marishume@gmail.com>
2025-03-18 13:53:33 -0300

    Add project files.

---
+++ Webd3000testing/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace Webd3000testing
+{
+    public class Contact
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ Webd3000testing/Controllers/ContactsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webd3000testing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+
+        private readonly ILogger<ContactsController> _logger;
+        private readonly IConfiguration _configuration;
+        
+
+        public ContactsController(ILogger<ContactsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from the Contacts Controller - GET");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Post(Contact contact)
+        {
+            //Validation:
+            if (string.IsNullOrEmpty(contact.FirstName))
+            {
+                return BadRequest("First Name is invalid.");
+            }
+
+            //Validation:
+            if (string.IsNullOrEmpty(contact.LastName))
+            {
+                return BadRequest("Last Name is invalid.");
+            }
+
+
+
+
+
+            return Ok("Hello" + contact.FirstName + "from the Contacts Controller - POST");
+        }
+
+
+
+
+
+
+
+
+    }
+}
